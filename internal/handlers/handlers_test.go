bd571944b3cdb1f864d2fcd4d201e7e92553991d
seiciana <aurel.seician@ppb.com>
2022-07-19 19:19:59 +0300

    Writing tests for POST handlers

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -26,16 +27,31 @@ var theTests = []struct {
 	{"search-availability", "/search-availability", "GET", []postData{}, http.StatusOK},
 	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
 	{"make-reservation", "/make-reservation", "GET", []postData{}, http.StatusOK},
+
+	{"post-search-avail", "/search-availability", "POST", []postData{
+		{key: "start", value: "2022-01-09"},
+		{key: "end", value: "2022-01-10"},
+	}, http.StatusOK},
+
+	{"post-search-avail-json", "/search-availability-json", "POST", []postData{
+		{key: "start", value: "2022-01-09"},
+		{key: "end", value: "2022-01-10"},
+	}, http.StatusOK}, {"make-reservation", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "Seician"},
+		{key: "last_name", value: "Aurel"},
+		{key: "email", value: "me@yahoo.com"},
+		{key: "phone", value: "555-555-555"},
+	}, http.StatusOK},
 }
 
 func TestHandlers(t *testing.T) {
 	routes := getRoutes()
-	ts := httptest.NewTLSServer(routes)
-	defer ts.Close()
+	testServer := httptest.NewTLSServer(routes)
+	defer testServer.Close()
 
 	for _, e := range theTests {
 		if e.method == "GET" {
-			response, err := ts.Client().Get(ts.URL + e.url)
+			response, err := testServer.Client().Get(testServer.URL + e.url)
 			if err != nil {
 				t.Log(err)
 				t.Fatal(err)
@@ -44,7 +60,18 @@ func TestHandlers(t *testing.T) {
 				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, response.StatusCode)
 			}
 		} else {
-
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+			response, err := testServer.Client().PostForm(testServer.URL+e.url, values)
+			if err != nil {
+				t.Log(err)
+				t.Fatal(err)
+			}
+			if response.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, response.StatusCode)
+			}
 		}
 	}
 }
